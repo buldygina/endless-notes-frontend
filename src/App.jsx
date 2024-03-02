0a4ea25059326c6be750a88d91629cf2c3c3c06c
buldygina <el.buldygina@mail.ru>
2024-03-02 23:09:25 +0300

    feat(main): vite init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+
+function App() {
+
+  return (
+    <IonPage>
+        <IonContent>
+            <IonButton>hello</IonButton>
+        </IonContent>
+    </IonPage>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {CookiesProvider} from "react-cookie";
+import {IonApp, IonRouterOutlet, setupIonicReact} from "@ionic/react";
+import {IonReactRouter} from "@ionic/react-router";
+import {Route} from "react-router-dom";
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/core.css'
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+setupIonicReact()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <CookiesProvider>
+            <IonApp>
+                <IonReactRouter>
+                    <IonRouterOutlet>
+                        <Route path={"/"} component={App}/>
+                    </IonRouterOutlet>
+                </IonReactRouter>
+            </IonApp>
+        </CookiesProvider>
+    </React.StrictMode>,
+)
