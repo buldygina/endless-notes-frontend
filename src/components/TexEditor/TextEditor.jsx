97ebf7728281745505282071782a224c7aa87dc3
sirok1 <s1r0k.dev@gmail.com>
2024-03-09 23:06:09 +0300

    feat(dev): module classes -> just classes

--- src/components/TexEditor/TextEditor.jsx
+++ src/components/TexEditor/TextEditor.jsx
@@ -1,11 +1,11 @@
-import cl from "./textEditor.module.css"
+
 
 const TextEditor = () => {
 	return (
 		<>
-			<textarea className={cl.editor__aria} placeholder="Начните что-то писать..."/>
-			<div className={cl.editor__toolbar__container}>
-				<div className={cl.editor__toolbar}>
+			<textarea className={"editor__aria"} placeholder="Начните что-то писать..."/>
+			<div className={"editor__toolbar__container"}>
+				<div className={"editor__toolbar"}>
 					тут будут кнопки
 				</div>
 			</div>
