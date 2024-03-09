e2c870fe9e16e619d727b1acc5e67123d38e40e7
sirok1 <s1r0k.dev@gmail.com>
2024-03-09 23:04:10 +0300

    feat(dev): theme toggle

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -11,45 +11,58 @@ import {
 } from "@ionic/react";
 import {arrowBackOutline, enterOutline} from "ionicons/icons";
 import person from "../assets/person-circle-sharp .png"
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
+
 const Account = () => {
-	const router = useHistory()
-	const backToMainPage = () => {
-		router.replace("/tabs/notes")
-	}
-	const logOut = () => {
-		router.replace("/login")
-	}
+    const router = useHistory()
+    const backToMainPage = () => {
+        router.replace("/tabs/notes")
+    }
+    const logOut = () => {
+        router.replace("/login")
+    }
+    const toggleTheme = (value) => {
+        document.body.classList.toggle("dark")
+    }
     return (
         <IonPage>
             <IonContent>
                 <div className={"headerAcc"}>
-            <IonButton onClick={backToMainPage} fill={'clear'} color={'dark'}> <IonIcon slot={"icon-only"} icon={arrowBackOutline}></IonIcon></IonButton>
-            <div className={"account"}><img src={person} alt=""/></div>
-            <IonButton onClick={logOut} fill={'clear'} color={'dark'}> <IonIcon slot={"icon-only"} icon={enterOutline}></IonIcon></IonButton>
+                    {/*<IonButton onClick={backToMainPage} fill={'clear'} color={'dark'}> <IonIcon slot={"icon-only"} icon={arrowBackOutline}></IonIcon></IonButton>*/}
+                    <div className={"account"}>
+                        <img src={person} alt=""/>
+                    </div>
+                    <IonButton className={"logoutButton"} onClick={logOut} fill={'clear'} color={'dark'}>
+                        <IonIcon slot={"icon-only"} icon={enterOutline}/>
+                    </IonButton>
                 </div>
                 <div className={"accountLog"}>
-                <IonList>
-                    <IonItem>
-                        <IonInput label="Username" placeholder="Enter your username" disabled/>
-                    </IonItem>
-                    <IonItem>
-                        <IonInput label="Email input" type="email" placeholder="email@domain.com" disabled/>
-                    </IonItem>
-                    <IonItem>
-                        <IonInput label="Password input" type="password" value="password" disabled/>
-                    </IonItem>
-                </IonList>
+                    <IonList>
+                        <IonItem>
+                            <IonInput label="Логин" placeholder="Ваше имя   " disabled/>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Почта" type="email" placeholder="email@domain.com" disabled/>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Пароль" type="password" value="password" disabled/>
+                        </IonItem>
+                    </IonList>
                 </div>
                 <div className={"theme"}>
-                <IonList>
-                    <IonItem>
-                        <IonSelect aria-label="Light/Dark theme" interface="popover" placeholder="Select theme">
-                            <IonSelectOption value="light">Light theme </IonSelectOption>
-                            <IonSelectOption value="dark">Dark theme</IonSelectOption>
-                        </IonSelect>
-                    </IonItem>
-                </IonList>
+                    <IonList>
+                        <IonItem>
+                            <IonSelect
+                                aria-label="Light/Dark theme"
+                                interface="popover"
+                                placeholder="Выбрать тему"
+                                onIonChange={(e) => toggleTheme(e.detail.value)}
+                            >
+                                <IonSelectOption value="light">Тёмная</IonSelectOption>
+                                <IonSelectOption value="dark">Светлая</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                    </IonList>
                 </div>
             </IonContent>
         </IonPage>
